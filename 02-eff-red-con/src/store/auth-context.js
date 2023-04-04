f70af523200bd41b871976b1548d3e36530bb532
Harshit Satya <hsatya@systango.com>
2023-04-04 19:40:50 +0530

    completed section for ref-effect-reducer

---
+++ 02-eff-red-con/src/store/auth-context.js
@@ -0,0 +1,43 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+const AuthContext = createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+  useEffect(() => {
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, [storedUserLoggedInInformation]);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem('isLoggedIn', '1');
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
